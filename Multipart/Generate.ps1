7391017e371a87cef996c1ce6e828a8f9f442a62
BabakBN <BabiBN@hotmail.com>
2025-04-19 15:50:21 +0330

    Build Multipart

---
+++ Multipart/Generate.ps1
@@ -0,0 +1,6 @@
+cd   $PSScriptRoot
+rm requirements_?.txt -Verbose ;
+cat .\requirements.freeze | % { echo  $_ >>"requirements_$($_.ToLower().ToChararray()[0]).txt" }
+
+
+"a".."z"|%{"RUN [ ! -f requirements_$_.txt ] || pip install   --no-cache-dir -r requirements_$_.txt"}
